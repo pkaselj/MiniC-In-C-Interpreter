351bf5a46998f9958e9e51ac7380984e981baeae
Petar Kaselj <petar.kaselj.00@gmail.com>
2026-03-01 22:31:05 +0100

    Added ast node definition

--- Parser.h
+++ Parser.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stddef.h>
+#include "List.h"
+#include "Lexer.h"
 
 // --- Forward declarations
 typedef struct TokenList TokenList;
@@ -32,11 +34,91 @@ struct AstNode
 	AstNodeType type;
 	union
 	{
-		 // TODO: Create Generic List in C
+		// List === List<AstNode>
 		struct
 		{
-			int _;
-		} ast_program;
+			struct AstNode* symbol;
+			List* args;
+		} fn_call;
+
+		struct
+		{
+			struct AstNode* left;
+			struct AstNode* right;
+		} assign;
+
+		struct
+		{
+			struct AstNode* child;
+			TokenType Op;
+		} unary_expr;
+
+		struct
+		{
+			struct AstNode* right;
+			struct AstNode* left;
+			TokenType op;
+		} binary_expr;
+
+		struct
+		{
+			StringView value;
+		} identifier;
+
+		struct
+		{
+			float value;
+		} number;
+
+		struct
+		{
+			StringView value;
+		} string;
+
+		struct
+		{
+			struct AstNode* expression;
+		} expr_stmt;
+
+		struct
+		{
+			struct AstNode* condition;
+			struct AstNode* block_if;
+			struct AstNode* block_else;
+		} if_stmt;
+
+		struct
+		{
+			struct AstNode* intial;
+			struct AstNode* end_condition;
+			struct AstNode* next_action;
+			struct AstNode* block;
+		} for_stmt;
+
+		struct
+		{
+			struct AstNode* condition;
+			struct AstNode* block;
+		} while_stmt;
+
+		struct
+		{
+			List* statements;
+		} block;
+
+		struct
+		{
+			struct AstNode* block;
+			struct AstNode* params;
+			struct AstNode* symbol;
+		} fn_def;
+
+		struct
+		{
+			List* function_definitions;
+			List* statements;
+		} program;
+
 	} u;
 };
 
